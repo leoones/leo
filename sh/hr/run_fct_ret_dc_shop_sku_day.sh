5fa8f08fbd637286eb475534cfca67aac0e522ca
lzy305 <38307479+lzy305@users.noreply.github.com>
2019-03-15 14:03:07 +0800

    Update run_fct_ret_dc_shop_sku_day.sh

--- sh/hr/run_fct_ret_dc_shop_sku_day.sh
+++ sh/hr/run_fct_ret_dc_shop_sku_day.sh
@@ -52,3 +52,7 @@ sql="
       and ret.outcheckdate  < addMonths(toDate('2018-01-01'), 1)
     ) dd
   all left  join dw_hr.dw_good_info dgi on dd.out_buid = dgi.buid and dd.goodsid = toUInt32(dgi.goodsid);"
+clickhouse-client -h 192.168.89.102 --user=default --password=sMNl+f/n -m -n --query="$sql"
+echo "----------------- $stat_mon Complete---------------------"
+startDate=$(date -d "$startDate+1months" +%Y%m%d)
+done
