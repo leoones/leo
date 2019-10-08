7cf53d3686db0163dabac3800dd1bff6964c9184
lzy305 <38307479+lzy305@users.noreply.github.com>
2019-10-08 20:03:25 +0800

    Update auto_upgrade_clickhouse.sh

--- 运维/auto_upgrade_clickhouse.sh
+++ 运维/auto_upgrade_clickhouse.sh
@@ -9,24 +9,28 @@ hosts=(${machines//,/ })
 #上传安装文件至指定服务器上
 for host in ${hosts[@]}
 do
- # ssh  $host "ls -alt /soft/clickhouse-*${version}_*.deb"
- #if [ $? -ne 0  ];then
-  echo '>>>>>>>>>>>>> 开始上传安装文件至机器:$host <<<<<<<<<<<<<<<'
+ echo $host
+ ssh  $host "ls -alt /soft/clickhouse-*${version}_*.deb"
+ if [ $? -ne 0  ];then
+  echo '>>>>>>>>>>>>> 版本:$version 上传安装文件开始 <<<<<<<<<<<<<<<'
   scp -r $soft_path/clickhouse-common-static_${version}_amd64.deb $host:$soft_path
   scp -r $soft_path/clickhouse-client_${version}_all.deb $host:$soft_path
   scp -r $soft_path/clickhouse-server_${version}_all.deb $host:$soft_path
-  echo '>>>>>>>>>>>>> 完成上传安装文件至机器:$host <<<<<<<<<<<<<<<<<<<'
- #fi
+  echo '>>>>>>>>>>>>>版本:$version 上传安装完成 <<<<<<<<<<<<<<<<<<<'
+ fi
 done
+
 
 
 for host in ${hosts[@]}
 do
-  echo ">>>>>>>>>>>>机器：$host 升级clickhouse 版本:$version"
-  ssh $host "service clickhouse-server stop && \
-             dpkg -i $soft_path/clickhouse-common-static_${version}_amd64.deb &&\
+  echo $host
+  echo ">>>>>>>>>>>> 升级clickhouse 版本:$version"
+  ssh $host "service clickhouse-server stop"
+
+  ssh $host "dpkg -i $soft_path/clickhouse-common-static_${version}_amd64.deb &&\
              dpkg -i $soft_path/clickhouse-server_${version}_all.deb &&\
-             dpkg -i $soft_path/clickhouse-client_${version}_all.deb &&\
-             service clickhouse-server start
+             dpkg -i $soft_path/clickhouse-client_${version}_all.deb
              "
+  ssh $host "service clickhouse-server start"
 done
